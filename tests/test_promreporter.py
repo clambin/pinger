69361ceb06f4fcd589e1d900df85f874f51eb02d
Christophe Lambin <christophe.lambin@gmail.com>
2020-02-27 00:35:28 +0100

    remove dev dependencies from runtime env

--- tests/test_promreporter.py
+++ tests/test_promreporter.py
@@ -1,26 +1,11 @@
 from metrics.reporter import PrometheusReporter
 
 
-class UnittestReporter(PrometheusReporter):
-    def __init__(self, port=8080):
-        super().__init__(port)
-        self.last = {}
-
-    def report(self, probe, val):
-        super().report(probe, val)
-        self.last[probe] = val
-
-    def measured(self, probe):
-        return self.last[probe]
-
-
 def test_bad_port():
-    reporter = UnittestReporter(12)
+    reporter = PrometheusReporter(12)
     try:
         reporter.start()
         assert False
-    except OSError as err:
-        pass
     # TODO: what exceptions does start_http_server raise?
     except Exception as err:
         pass

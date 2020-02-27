6c5c533dcbaa43e872b03465c7d9fd7326ba6610
Christophe Lambin <christophe.lambin@gmail.com>
2020-02-27 01:11:12 +0100

    remove dev dependencies from runtime env

--- tests/test_pinger.py
+++ tests/test_pinger.py
@@ -1,7 +1,10 @@
 import argparse
+import os
 from pinger import pinger
 
 
 def test_pinger():
-    config = argparse.Namespace(interval=5, port=8080, once=True, logfile=None, debug=True, hosts=['localhost'])
+    config = argparse.Namespace(interval=5, port=8080, once=True, logfile='logfile.txt', debug=True,
+                                hosts=['localhost'])
     assert pinger(config) == 0
+    os.remove('logfile.txt')

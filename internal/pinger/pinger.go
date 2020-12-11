de2c5f9f39a6caf4afd9526ab0273ccdd3711583
Christophe Lambin <christophe.lambin@gmail.com>
2020-12-11 14:58:11 +0100

    golang version

--- internal/pinger/pinger.go
+++ internal/pinger/pinger.go
@@ -11,54 +11,67 @@ import (
 	"pinger/internal/pingtracker"
 )
 
+type pingFunc func(string, *pingtracker.PingTracker)
+
 func Run(hosts []string, interval time.Duration) {
-	RunNTimes(hosts, interval, -1)
+	RunNTimes(hosts, interval, -1, pinger)
 }
 
-func RunNTimes(hosts []string, interval time.Duration, passes int) {
+func RunNTimes(hosts []string, interval time.Duration, passes int, pinger pingFunc) (int, int, time.Duration) {
 	var trackers = make(map[string]*pingtracker.PingTracker, len(hosts))
 
 	for _, host := range hosts {
 		trackers[host] = pingtracker.New()
 
-		go func(host string) {
-			pinger, err := ping.NewPinger(host)
-			if err != nil {
-				panic(err)
-			}
-
-			if runtime.GOOS == "linux" {
-				pinger.SetPrivileged(true)
-			}
-
-			pinger.Interval = 5 * time.Second
-
-			pinger.OnRecv = func(pkt *ping.Packet) {
-				log.Debugf("%s: seq nr %d, latency %v", host, pkt.Seq, pkt.Rtt)
-				trackers[host].Track(pkt.Seq, pkt.Rtt)
-			}
-			if err = pinger.Run(); err != nil {
-				panic(err)
-			}
-		}(host)
+		go pinger(host, trackers[host])
 	}
+
+	totalCount := 0
+	totalLoss := 0
+	totalLatency := int64(0)
 
 	for {
 		if passes != -1 {
-			passes--
 			if passes == 0 {
 				break
 			}
+			passes--
 		}
 
 		time.Sleep(interval)
 
 		for name, tracker := range trackers {
 			count, loss, latency := tracker.Calculate()
-
 			metrics.Measure(name, count, loss, latency)
 
 			log.Debugf("%s: received: %d, loss: %d, latency:%v", name, count, loss, latency)
+
+			totalCount += count
+			totalLoss += loss
+			totalLatency += latency.Nanoseconds()
 		}
+	}
+
+	return totalCount, totalLoss, time.Duration(totalLatency)
+}
+
+func pinger(host string, tracker *pingtracker.PingTracker) {
+	pinger, err := ping.NewPinger(host)
+	if err != nil {
+		panic(err)
+	}
+
+	if runtime.GOOS == "linux" {
+		pinger.SetPrivileged(true)
+	}
+
+	pinger.Interval = 5 * time.Second
+
+	pinger.OnRecv = func(pkt *ping.Packet) {
+		log.Debugf("%s: seq nr %d, latency %v", host, pkt.Seq, pkt.Rtt)
+		tracker.Track(pkt.Seq, pkt.Rtt)
+	}
+	if err = pinger.Run(); err != nil {
+		panic(err)
 	}
 }
--- internal/pinger/pinger_test.go
+++ internal/pinger/pinger_test.go
@@ -1,18 +1,28 @@
 package pinger
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
+
+	"pinger/internal/pingtracker"
 )
 
 func TestPinger(t *testing.T) {
 
-	// f, err := os.Create("cpuprofile")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// _ = pprof.StartCPUProfile(f)
-	// defer pprof.StopCPUProfile()
+	hosts := []string{"127.0.0.1"}
+	count, loss, latency := RunNTimes(hosts, 1*time.Second, 5, testPinger)
 
-	// hosts := []string{"127.0.0.1"}
-	// RunNTimes(hosts, 2*time.Second, 5)
+	assert.Greater(t, count, 9)
+	assert.Equal(t, 1, loss)
+	assert.Equal(t, time.Duration(int64(count*50*1000000)), latency)
+}
+
+func testPinger(_ string, tracker *pingtracker.PingTracker) {
+	seqNo := 1
+	for {
+		tracker.Track(seqNo, 50*time.Millisecond)
+		seqNo++
+		time.Sleep(500 * time.Millisecond)
+	}
 }

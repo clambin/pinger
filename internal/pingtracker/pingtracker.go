2cc271aa1f17a433b1a077079ad1fd64d2362bd0
Christophe Lambin <christophe.lambin@gmail.com>
2020-12-13 00:16:30 +0100

    more efficient(?) gap detection

--- internal/pingtracker/pingtracker.go
+++ internal/pingtracker/pingtracker.go
@@ -65,32 +65,34 @@ func (tracker *PingTracker) calculateLoss() int {
 	// Sort all sequence numbers and remove duplicates
 	sort.Ints(tracker.seqNrs)
 	unique.Ints(&tracker.seqNrs)
+
 	// sequence numbers can wrap around!
 	// In this case, we'd get something like [ 0, 1, 2, 3, 65534, 65535 ]
-	//Split into two slices [ 65534, 65535 ] and [ 0, 1, 2 ] using nextSeqNr as a boundary
-	// Process the higher slice first (pre-wrap) and then the lower one (post-wrap)
-	// TODO: avoid allocating new slices: determine the index where 'higher' starts and work w/ sub-slices instead
-	higher := make([]int, 0)
-	lower := make([]int, 0)
-	for _, seqNr := range tracker.seqNrs {
-		if seqNr >= tracker.NextSeqNr {
-			higher = append(higher, seqNr)
-		} else if seqNr < tracker.NextSeqNr-10000 {
-			lower = append(lower, seqNr)
+	// Split into two lists [ 65534, 65535 ] and [ 0, 1, 2 ] using nextSeqNr as a boundary
+	// Process the higher list first (pre-wrap) and then the lower one (post-wrap)
+	index := 0
+	for ; index < len(tracker.seqNrs); index++ {
+		if tracker.seqNrs[index] >= tracker.NextSeqNr-60 { // Allow up to 60 packets (1 min) of old packets
+			break
 		}
-
 	}
 	total := 0
-	if len(higher) > 0 {
-		total = higher[0] - tracker.NextSeqNr
-		total += countGaps(higher)
-		tracker.NextSeqNr = higher[len(higher)-1] + 1
+	// pre-wrap
+	if index != len(tracker.seqNrs) {
+		total = tracker.seqNrs[index] - tracker.NextSeqNr
+		// old packets
+		if total < 0 {
+			total = 0
+		}
+		total += countGaps(tracker.seqNrs[index:])
+		tracker.NextSeqNr = tracker.seqNrs[len(tracker.seqNrs)-1] + 1
 	}
-	if len(lower) > 0 {
+	// post-wrap
+	if index > 0 {
 		tracker.NextSeqNr = 0
-		total += lower[0] - tracker.NextSeqNr
-		total += countGaps(lower)
-		tracker.NextSeqNr = lower[len(lower)-1] + 1
+		total += tracker.seqNrs[0] // - tracker.NextSeqNr
+		total += countGaps(tracker.seqNrs[:index])
+		tracker.NextSeqNr = tracker.seqNrs[index-1] + 1
 	}
 
 	return total

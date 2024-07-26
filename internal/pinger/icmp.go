5f32d6797db1e9f146303e76e19c6d79ab4f3be4
Christophe Lambin <christophe.lambin@gmail.com>
2024-07-26 16:40:40 +0200

    refactor

---
+++ internal/pinger/icmp.go
@@ -0,0 +1,160 @@
+package pinger
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
+	"golang.org/x/net/ipv6"
+	"golang.org/x/sync/errgroup"
+	"log/slog"
+	"net"
+	"time"
+)
+
+type Transport int
+
+const (
+	IPv4 Transport = 0x01
+	IPv6 Transport = 0x02
+)
+
+func (tp Transport) String() string {
+	switch tp {
+	case IPv4:
+		return "ipv4"
+	case IPv6:
+		return "ipv6"
+	default:
+		return "unknown"
+	}
+}
+
+type icmpSocket struct {
+	Timeout time.Duration
+	OnReply func(net.IP, *icmp.Echo)
+	v4      *icmp.PacketConn
+	v6      *icmp.PacketConn
+	logger  *slog.Logger
+}
+
+func newICMPSocket(l *slog.Logger) *icmpSocket {
+	s := icmpSocket{
+		Timeout: 5 * time.Second,
+		logger:  l,
+	}
+	s.v4, _ = icmp.ListenPacket("udp4", "0.0.0.0")
+	s.v6, _ = icmp.ListenPacket("udp6", "::")
+	return &s
+}
+
+func (s *icmpSocket) ping(ip net.IP, seq int, payload []byte) error {
+	var socket *icmp.PacketConn
+	tp := getTransport(ip)
+	switch tp {
+	case IPv4:
+		socket = s.v4
+	case IPv6:
+		socket = s.v6
+	}
+
+	if socket == nil {
+		return fmt.Errorf("icmp socket does not support %s", tp)
+	}
+
+	msg := echoRequest(tp, seq, payload)
+	data, _ := msg.Marshal(nil)
+	_, err := socket.WriteTo(data, &net.UDPAddr{IP: ip})
+	return err
+}
+
+func (s *icmpSocket) listen(ctx context.Context) error {
+	var g errgroup.Group
+	if s.v4 != nil {
+		g.Go(func() error { return s.serve(ctx, s.v4, IPv4) })
+	}
+	if s.v6 != nil {
+		g.Go(func() error { return s.serve(ctx, s.v6, IPv6) })
+	}
+	<-ctx.Done()
+	return g.Wait()
+}
+
+func (s *icmpSocket) serve(ctx context.Context, c *icmp.PacketConn, tp Transport) error {
+	ticker := time.NewTicker(500 * time.Millisecond)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ticker.C:
+			var err error
+			for err == nil {
+				err = s.readReply(c, tp)
+				//s.logger.Info("rcvd", "err", err)
+				var terr net.Error
+				if err != nil && (!errors.As(err, &terr) || !terr.Timeout()) {
+					s.logger.Warn("failed to receive ICMP packet", "err", err)
+				}
+			}
+		case <-ctx.Done():
+			return nil
+		}
+	}
+}
+
+func (s *icmpSocket) readReply(c *icmp.PacketConn, tp Transport) error {
+	if err := c.SetReadDeadline(time.Now().Add(s.Timeout)); err != nil {
+		s.logger.Warn("failed to set deadline", "err", err)
+	}
+	rb := make([]byte, 1500)
+	count, from, err := c.ReadFrom(rb)
+	if err != nil {
+		return err
+	}
+	if s.OnReply == nil {
+		return nil
+	}
+	msg, err := echoReply(rb[:count], tp)
+	if err != nil {
+		s.logger.Warn("Failed to parse ICMP message", "err", err)
+	} else if msg.Type == ipv4.ICMPTypeEchoReply || msg.Type == ipv6.ICMPTypeEchoReply {
+		s.OnReply(from.(*net.UDPAddr).IP, msg.Body.(*icmp.Echo))
+	}
+	return nil
+}
+
+func getTransport(ip net.IP) Transport {
+	if len(ip.To4()) == 0 {
+		return IPv6
+	}
+	return IPv4
+}
+
+var echoRequestTypes = map[Transport]icmp.Type{
+	IPv4: ipv4.ICMPTypeEcho,
+	IPv6: ipv6.ICMPTypeEchoRequest,
+}
+
+func echoRequest(tp Transport, seq int, payload []byte) icmp.Message {
+	return icmp.Message{
+		Type: echoRequestTypes[tp],
+		Code: 0,
+		Body: &icmp.Echo{
+			ID:   1,
+			Seq:  seq,
+			Data: payload,
+		},
+	}
+}
+
+func echoReply(data []byte, tp Transport) (*icmp.Message, error) {
+	var protocol int
+	if tp&IPv4 != 0 {
+		protocol = 1
+	} else if tp&IPv6 != 0 {
+		protocol = 58
+	} else {
+		return nil, fmt.Errorf("unknown protocol: %d", tp)
+	}
+	return icmp.ParseMessage(protocol, data)
+}
